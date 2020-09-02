3e4bdb627a8e857693c62465aafb0befb890a031
Julian Yap <julianokyap@gmail.com>
2020-09-01 21:54:11 -0700

    Remove settings mobile sync, 3Box, IPFS gateway

--- ui/app/pages/settings/advanced-tab/advanced-tab.component.js
+++ ui/app/pages/settings/advanced-tab/advanced-tab.component.js
@@ -472,7 +472,6 @@ export default class AdvancedTab extends PureComponent {
       <div className="settings-page__body">
         { warning && <div className="settings-tab__error">{ warning }</div> }
         { this.renderStateLogs() }
-        { this.renderMobileSync() }
         { this.renderResetAccount() }
         { this.renderAdvancedGasInputInline() }
         { this.renderTransactionTimeEstimates() }
@@ -480,8 +479,6 @@ export default class AdvancedTab extends PureComponent {
         { this.renderShowConversionInTestnets() }
         { this.renderUseNonceOptIn() }
         { this.renderAutoLockTimeLimit() }
-        { this.renderThreeBoxControl() }
-        { this.renderIpfsGatewayControl() }
       </div>
     )
   }
