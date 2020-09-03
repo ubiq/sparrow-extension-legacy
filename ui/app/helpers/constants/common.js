59a246ff23546acbebfdec943a9e91b74cecb34a
Luke Williams <luke@iquidus.io>
2020-09-03 03:18:33 +0200

    dont treat as testnet (price conversions)

--- ui/app/helpers/constants/common.js
+++ ui/app/helpers/constants/common.js
@@ -6,7 +6,7 @@ export const PRIMARY = 'PRIMARY'
 export const SECONDARY = 'SECONDARY'
 
 export const NETWORK_TYPES = {
-  KOVAN: 'kovan',
+  MAINNET: 'mainnet',
 }
 
 export const GAS_ESTIMATE_TYPES = {
