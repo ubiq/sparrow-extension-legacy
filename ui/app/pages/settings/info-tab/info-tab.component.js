3c096f45750161adb97796582a40ed510514bcaa
Julian Yap <julianokyap@gmail.com>
2020-11-02 14:14:02 -0800

    Add Privacy policy

--- ui/app/pages/settings/info-tab/info-tab.component.js
+++ ui/app/pages/settings/info-tab/info-tab.component.js
@@ -19,6 +19,17 @@ export default class InfoTab extends PureComponent {
         <div className="info-tab__link-header">
           { t('links') }
         </div>
+        <div className="info-tab__link-item">
+          <Button
+            type="link"
+            href="https://ubiqsmart.com/sparrow-privacy"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="info-tab__link-text"
+          >
+            { t('privacyMsg') }
+          </Button>
+        </div>
         <hr className="info-tab__separator" />
         <div className="info-tab__link-item">
           <Button
