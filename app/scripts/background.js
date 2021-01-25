d4a986af0ba5823f93749fa75f3101cd68a9f26a
Jaimish Patel <jaimish.patel95@gmail.com>
2021-01-24 22:29:39 -0500

    Prevent sparrow from opening multiple windows

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -55,6 +55,7 @@ const sentry = setupSentry({ release })
 
 let popupIsOpen = false
 let notificationIsOpen = false
+let uiIsTriggering = false
 const openMetamaskTabsIDs = {}
 const requestAccountTabIds = {}
 
@@ -427,10 +428,18 @@ function setupController (initState, initLangCode) {
  * Opens the browser popup for user confirmation
  */
 async function triggerUi () {
-  const tabs = await platform.getActiveTabs()
-  const currentlyActiveMetamaskTab = Boolean(tabs.find((tab) => openMetamaskTabsIDs[tab.id]))
-  if (!popupIsOpen && !currentlyActiveMetamaskTab) {
-    await notificationManager.showPopup()
+  if (uiIsTriggering) return;
+  uiIsTriggering = true;
+  try {
+    const tabs = await platform.getActiveTabs()
+    const currentlyActiveMetamaskTab = Boolean(tabs.find((tab) => openMetamaskTabsIDs[tab.id]))
+    if (!popupIsOpen && !currentlyActiveMetamaskTab) {
+      await notificationManager.showPopup()
+    }
+  } catch (error) {
+    log.error('Sparrow - Trigger UI failed', error);
+  } finally {
+    uiIsTriggering = false;
   }
 }
 
